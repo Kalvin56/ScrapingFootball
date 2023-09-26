db9eddb1e1317bd75a41b544abbe868626f481d8
kalvin <kalvin.daniel56@laposte.net>
2023-09-26 19:12:26 +0200

    add image in s3

--- api/code/Controller/News.js
+++ api/code/Controller/News.js
@@ -1,15 +1,46 @@
 const { News } = require("../Model/News");
+const { httpHF } = require("../axios");
+const { s3 } = require("../s3");
+const { generateUniqueKey } = require("../utils");
 
 const create = (req, res) => {
 	let creationurl = req.body.url;
-	News.findOneAndUpdate({url: creationurl},req.body, {
-		new: true,
-		upsert: true
-	})
-		.then((result) => res.status(201).json(result))
-		.catch((error) => res.status(400).json({
+	if(req.body?.text){
+		let bodySD = JSON.stringify({inputs: req.body.text});
+		// génération image model stable diffusion
+		httpHF.post('/models/runwayml/stable-diffusion-v1-5', bodySD, {responseType: 'arraybuffer'})
+		.then((response) => {
+			const buffer = Buffer.from(response.data);
+			key = `${generateUniqueKey()}.jpg`
+			const params = {
+				Bucket: 'scrapbucket',
+				Key: key,
+				Body: buffer,
+			};
+			// sauvegarde de l'image dans un bucket aws s3
+			s3.putObject(params, (err, data) => {
+				if (err) {
+					res.status(400).json({
+						message: "Erreur lors du stockage de l'image dans S3",
+					})
+				} else {
+					console.log('L\'image a été stockée avec succès dans S3');
+					newBody = {...req.body, s3key: key}
+					// ajout de la news en bdd
+					News.findOneAndUpdate({url: creationurl},newBody, {
+						new: true,
+						upsert: true
+					})
+						.then((result) => res.status(201).json(result))
+						.catch((error) => res.status(400).json({
+							message: error?.message || "Une erreur est survenue",
+						}))
+				}
+			});
+		}).catch((error) => res.status(400).json({
 			message: error?.message || "Une erreur est survenue",
 		}))
+	}
 }
 
 const getId = (req, res) => {
