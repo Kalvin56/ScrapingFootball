9b05140f7d9fbc54ab5832716c025960932067a7
JulesDrouet <jules.drouet44@gmail.com>
2023-09-26 16:14:19 +0200

    petit commit tranquilou bilou

--- script/Scraping/Program.cs
+++ script/Scraping/Program.cs
@@ -10,18 +10,14 @@ class Program
 
         var articlesLequipe = await FonctionsScrap.ScrapLequipe();
         var articlesOneFootball = await FonctionsScrap.ScrapOneFootball();
-        Console.WriteLine(articlesLequipe.Count);
+
         foreach (var article in articlesLequipe)
         {
             await FonctionsScrap.SaveArticle(article.url, article.text);
-            Console.WriteLine(article.url);
-            Console.WriteLine(article.text);
         }
         foreach (var article in articlesOneFootball)
         {
             await FonctionsScrap.SaveArticle(article.url, article.text);
-            Console.WriteLine(article.url);
-            Console.WriteLine(article.text);
         }
     }
 
